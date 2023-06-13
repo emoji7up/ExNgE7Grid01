ce57075322b60a0580abf4ba6fdc4214527dcb4e
emoji7up <38649821+emoji7up@users.noreply.github.com>
2023-06-13 07:02:54 +0200

    3 files added

---
+++ src/kanban-requests/kanban-requests.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kanban-requests',
+  templateUrl: './kanban-requests.component.html',
+  styleUrls: ['./kanban-requests.component.css']
+})
+export class KanbanRequestsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
